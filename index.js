b06ef3c663ed4aa36e366f0bb8e88a655670f77f
unil19 <1294666380@qq.com>
2017-08-21 17:31:16 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-let a = "hello";
-let b = "git";
-let c = a + b;
-console.log(c);
-
-let d = a+","+b;
+var axios = require("axios");
+axios.post('http://localhost:9999/invoke', {
+	fcn: 'fcn',
+	args: 'args1,args2'
+	})
+	.then(function (response) {
+		console.log(response);
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
+// axios.post('http://localhost:9999/query', {
+// 	fcn: 'fcn',
+// 	args: 'args1,args2'
+// 	})
+// 	.then(function (response) {
+// 		console.log(response);
+// 	})
+// 	.catch(function (error) {
+// 		console.log(error);
+// 	});
